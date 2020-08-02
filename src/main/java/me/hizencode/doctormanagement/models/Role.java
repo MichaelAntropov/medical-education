8d739ba4155ce9c85de11ffe9fca5df907073cc6
Michael <mikhael.antropov@gmail.com>
2020-08-02 17:46:41 +0200

    Initial commit

---
+++ src/main/java/me/hizencode/doctormanagement/models/Role.java
@@ -0,0 +1,40 @@
+package me.hizencode.doctormanagement.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(schema = "doctor_management", name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
